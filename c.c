5fb8e715bc1561aa4733e020cab1346746f678f9
abijith <tagmeiba@gmail.com>
2021-08-22 23:19:00 +0530

    addition

---
+++ c.c
@@ -0,0 +1,11 @@
+#inlcude<iostream.h>
+using namespace std;
+{
+int sum,num1,num2;
+cout<<"Enter two numbers :";
+cin>>num1>>num2;
+sum=num1+num2;
+cout<<"Sum of the numbers are:"<<sum;
+return 0;
+
+}
