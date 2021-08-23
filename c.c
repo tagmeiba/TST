6e7cba1a5b864eab58cf0ccc68c18da1bef787e4
Abi <tagmeiba@gmail.com>
2021-08-23 12:13:15 +0530

    Modified program to addition standard

--- c.c
+++ c.c
@@ -2,11 +2,6 @@
 using namespace std;
 {
 int sum,num1,num2;
-cout<<"Message enered";
-cout<<"hai";
-cout<<"New added";
-cout<<"Changed";
-cout<<"Change for git hub";
 cout<<"Enter two numbers :";
 cin>>num1>>num2;
 sum=num1+num2;
